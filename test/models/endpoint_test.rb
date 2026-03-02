7dbf9da9d6ddf660ad1ba15f49503ebc7baaf207
Pawel Gorczyca <goreck888@gmail.com>
2026-03-02 05:11:30 +0100

    fix: Implement exchange token
    Allow other applications to exchange token
    with the same identity providers
    as connected with widgets.
    
    NOTICE:
    
    Probably needs identity provider configuration!

--- test/models/endpoint_test.rb
+++ test/models/endpoint_test.rb
@@ -5,7 +5,15 @@ require_relative '../test_helper'
 class EndpointTest < Minitest::Test
   def setup
     super
-    @idp = IdentityProvider.create!(name: 'IDP for Endpoint', url: 'https://idp.example.com')
+    @idp = IdentityProvider.create!(
+      name: 'IDP for Endpoint',
+      url: 'https://idp.example.com',
+      config: {
+        client_id: 'client123',
+        client_secret: 'secret123',
+        token_endpoint: 'https://keycloak.example.com/token'
+      }.to_json
+    )
   end
 
   def test_endpoint_creation
@@ -23,5 +31,24 @@ class EndpointTest < Minitest::Test
     refute endpoint.valid?
     assert_includes endpoint.errors[:name], "can't be blank"
     assert_includes endpoint.errors[:url], "can't be blank"
+  end
+
+  def test_exchange_token
+    endpoint = Endpoint.create!(
+      name: 'Test Endpoint',
+      url: 'https://endpoint.example.com',
+      abbreviation: 'WIDGET_A',
+      identity_provider: @idp
+    )
+
+    response_body = { 'access_token' => 'widget-token' }.to_json
+    response = Minitest::Mock.new
+    response.expect :is_a?, true, [Net::HTTPSuccess]
+    response.expect :body, response_body
+
+    Net::HTTP.stub :post_form, response do
+      result = endpoint.exchange_token('subject-token')
+      assert_equal 'widget-token', result['access_token']
+    end
   end
 end
--- test/models/identity_provider_test.rb
+++ test/models/identity_provider_test.rb
@@ -23,4 +23,56 @@ class IdentityProviderTest < Minitest::Test
     idp.update(name: 'Updated Name')
     idp.destroy
   end
+
+  def test_token_endpoint_from_config
+    idp = IdentityProvider.new(config: { token_endpoint: 'https://keycloak.example.com/token' }.to_json)
+    assert_equal 'https://keycloak.example.com/token', idp.token_endpoint
+  end
+
+  def test_token_endpoint_from_issuer
+    idp = IdentityProvider.new(config: { issuer: 'https://keycloak.example.com' }.to_json)
+    assert_equal 'https://keycloak.example.com/protocol/openid-connect/token', idp.token_endpoint
+  end
+
+  def test_exchange_token_success
+    idp = IdentityProvider.new(
+      slug: 'test-idp',
+      config: {
+        client_id: 'client123',
+        client_secret: 'secret123',
+        token_endpoint: 'https://keycloak.example.com/token'
+      }.to_json
+    )
+
+    response_body = { 'access_token' => 'new-token' }.to_json
+    response = Minitest::Mock.new
+    response.expect :is_a?, true, [Net::HTTPSuccess]
+    response.expect :body, response_body
+
+    Net::HTTP.stub :post_form, response do
+      result = idp.exchange_token('subject-token', audience: 'widget-audience')
+      assert_equal 'new-token', result['access_token']
+    end
+  end
+
+  def test_exchange_token_failure
+    idp = IdentityProvider.new(
+      slug: 'test-idp',
+      config: {
+        client_id: 'client123',
+        client_secret: 'secret123',
+        token_endpoint: 'https://keycloak.example.com/token'
+      }.to_json
+    )
+
+    response = Minitest::Mock.new
+    response.expect :is_a?, false, [Net::HTTPSuccess]
+    response.expect :code, '401'
+    response.expect :body, 'Unauthorized'
+
+    Net::HTTP.stub :post_form, response do
+      result = idp.exchange_token('subject-token')
+      assert_nil result
+    end
+  end
 end
